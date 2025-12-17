69f665550bca9859c8703f8abdcfe79d75310d67
Henry LeCompte <henry@lunar-glass.com>
2025-12-17 10:45:19 -0500

    Cleaned up project files Might be very bad practice but at least intellisene works now

--- bin/test_div_mod.ml
+++ bin/test_div_mod.ml
@@ -1,7 +1,8 @@
 open! Core
 open! Hardcaml
+open! Bits
 module Waveform = Hardcaml_waveterm.Waveform
-module Div_mod = Hardcaml_demo_project.Div_mod
+module Div_mod = Hardcaml_day1.Div_mod
 (* module Harness = Cyclesim_harness.Make (Range_finder.I) (Range_finder.O) *)
 
 let create_sim circuit =
@@ -14,12 +15,12 @@ let create_sim circuit =
   let divisor = Cyclesim.out_port sim "divisor" in
   let valid = Cyclesim.out_port sim "valid" in
   waves, sim, a, b, first, modulo, divisor, valid
+;;
 
 let test a_in b_in =
   let open Bits in
-  let waves, sim, a, b, first, modulo, divisor, valid = 
-    Div_mod.(create_circuit) (width a_in) (width b_in)
-    |> create_sim
+  let waves, sim, a, b, first, modulo, divisor, valid =
+    Div_mod.(create_circuit) (width a_in) (width b_in) |> create_sim
   in
   let step iteration =
     first := if iteration = 0 then vdd else gnd;
@@ -27,16 +28,22 @@ let test a_in b_in =
   in
   a := a_in;
   b := b_in;
-  for i=0 to width b_in - 1 do
+  for i = 0 to width b_in - 1 do
     step i
   done;
   let modulo = !modulo in
   let divisor = !divisor in
   Cyclesim.cycle sim;
   waves, modulo, divisor, !valid
+;;
 
-let waves, modulo, divisor, valid = test (Bits.of_signed_int ~width:5 (7)) (Bits.of_unsigned_int ~width:5 3);;
+let waves, modulo, divisor, valid = test (of_int ~width:5 7) (Bits.of_int ~width:5 3);;
 
-Stdio.printf "modulo: %i divisor: %i valid %b\n" (Bits.to_signed_int modulo) (Bits.to_signed_int divisor) (Bits.to_bool valid) ;;
+Stdio.printf
+  "modulo: %i divisor: %i valid %b\n"
+  (Bits.to_sint modulo)
+  (Bits.to_sint divisor)
+  (Bits.to_bool valid)
+;;
 
 Waveform.print waves
--- src/div_mod.ml
+++ src/div_mod.ml
@@ -1,14 +1,15 @@
 open! Core
 open! Hardcaml
+open! Bits
 open! Signal
 
-let partial_sub (a: t) (b: t) =
+let partial_sub (a : Signal.t) (b : Signal.t) =
   let width = Signal.width a in
-  let res = mux2 (a <+ of_signed_int ~width 0) (a +: b) (a -: b) in
-  mux2 ((a <+ b) &: (a >+ of_signed_int ~width 0)) a res
+  let res = mux2 (a <+ of_int ~width 0) (a +: b) (a -: b) in
+  mux2 (a <+ b &: (a >+ of_int ~width 0)) a res
 ;;
 
-let sub_counter_reg spec first a b = 
+let sub_counter_reg spec first a b =
   let width_a = width a in
   let modulo_w = wire width_a -- "running_modulo" in
   let mod_or_a = mux2 first a modulo_w in
@@ -17,15 +18,19 @@ let sub_counter_reg spec first a b =
   let modulo_r = reg spec ~enable:vdd modulo in
   let divisor_r = reg spec (mux2 (modulo <>: mod_or_a) (divisor_w +:. 1) divisor_w) in
   let valid_r = reg spec (mux2 (modulo <>: mod_or_a) gnd vdd) in
-  modulo_w <-- modulo_r;
-  divisor_w <-- divisor_r;
+  modulo_w <== modulo_r;
+  divisor_w <== divisor_r;
   modulo_r, divisor_r, valid_r
+;;
 
-let create_circuit a_width b_width = 
+let create_circuit a_width b_width =
   let clock = input "clock" 1 in
   let first = input "first" 1 in
   let a = input "a" a_width in
   let b = input "b" b_width in
   let spec = Reg_spec.create ~clock () in
-  let (modulo, divisor, valid) = sub_counter_reg spec first a b in
-  Circuit.create_exn ~name:"div_mod" [ output "modulo" modulo; output "divisor" divisor; output "valid" valid ];;
+  let modulo, divisor, valid = sub_counter_reg spec first a b in
+  Circuit.create_exn
+    ~name:"div_mod"
+    [ output "modulo" modulo; output "divisor" divisor; output "valid" valid ]
+;;
